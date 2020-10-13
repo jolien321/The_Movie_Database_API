0372a60642dd674f0994dd1b53941e3689cbc358
jolien321 <45165610+jolien321@users.noreply.github.com>
2020-10-13 17:51:15 +0200

    Add files via upload

---
+++ TheMovieDatabaseAPI.py
@@ -0,0 +1,32 @@
+#importeren van modules
+import urllib.parse
+import requests
+
+#variabelen benoemen + input geven
+main_api = "https://api.themoviedb.org/3/search/movie?"
+#vul je authenticatie credentials hier in
+key = ""
+while True:
+    title = input("Movie to search for: ")
+    #om het scriptje te kunenn stoppen
+    if title == "quit" or title == "q":
+            break
+
+
+    #url samenstellen
+    url = main_api + urllib.parse.urlencode({"api_key":key, "query":title})
+
+    #informatie ophalen via url
+    json_data = requests.get(url).json()
+
+    if json_data != "":
+        #resultaat printen
+        for item in json_data["results"]:
+            print("=================================================================")
+            print("Title: " + str(item["title"]) + "\n")
+            print("Popularity:", item["popularity"] + "\n")
+            print("Overview:", item["overview"] + "\n")
+            print("=================================================================")
+    #als er geen resultaat is gevonden
+    else:
+        print("This movie is not found")
